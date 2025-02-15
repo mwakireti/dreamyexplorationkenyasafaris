215a8b223f5019fe63cfcea119caa95b5152f80f
D94emil <dansonmwakireti@gmail.com>
2025-02-15 23:03:50 +0300

    addition layout features

---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+<!-- Footer  -->
+<footer id="footer_area">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="footer_heading_area">
+          <h5>Need any help?</h5>
+        </div>
+        <div class="footer_first_area">
+          <div class="footer_inquery_area">
+            <h5>Call 24/7 for any help</h5>
+            <h3> <a href="tel:+00-123-456-789">+255 023 456 789</a></h3>
+          </div>
+          <div class="footer_inquery_area">
+            <h5>Mail to our support team</h5>
+            <h3> <a href="mailto:support@nortazsafari.com">support@nortazsafari.com</a></h3>
+          </div>
+          <div class="footer_inquery_area">
+            <h5>Follow us on</h5>
+            <ul class="soical_icon_footer">
+              <li><a href="#!"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="#!"><i class="fab fa-twitter-square"></i></a></li>
+              <li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#!"><i class="fab fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2 offset-lg-1 col-md-6 col-sm-6 col-12">
+        <div class="footer_heading_area">
+          <h5>Company</h5>
+        </div>
+        <div class="footer_link_area">
+          <ul>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="tour-guides.php">Meet the Team </a></li>
+            <li><a href="news.php">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+        <div class="footer_heading_area">
+          <h5>Support</h5>
+        </div>
+        <div class="footer_link_area">
+          <ul>
+            <li><a href="testimonials">Legal</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="privacy-policy">Privacy Policy</a></li>
+          </ul>
+        </div>
+      </div>
+</footer>
+<div class="copyright_area">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="co-lg-6 col-md-6 col-sm-12 col-12">
+        <div class="copyright_left">
+          <p>Copyright © 2024 Nortazsafari ltd All Rights Reserved</p>
+        </div>
+      </div>
+      <div class="co-lg-6 col-md-6 col-sm-12 col-12">
+        <div class="copyright_right">
+          <img src="assets/img/common/cards.png" alt="img">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="go-top">
+  <i class="fas fa-chevron-up"></i>
+  <i class="fas fa-chevron-up"></i>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,236 @@
+    <header class="main_header_arae navbar_color_black">
+      <!-- Top Bar -->
+      <div class="topbar-area">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+              <ul class="topbar-list">
+                <li>
+                  <a href="#!"><i class="fab fa-facebook"></i></a>
+                  <a href="#!"><i class="fab fa-twitter-square"></i></a>
+                  <a href="#!"><i class="fab fa-instagram"></i></a>
+                  <a href="#!"><i class="fab fa-linkedin"></i></a>
+                </li>
+                <li><a href="#!"><span>+255 234 567 89</span></a></li>
+                <li><a href="Mailto:inquiry@nortazsafari.com"><span>inquiry@nortazsafari.com</span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Navbar Bar -->
+      <div class="navbar-area">
+        <div class="main-responsive-nav">
+          <div class="container">
+            <div class="main-responsive-menu">
+              <div class="logo">
+                <a href="home">
+                  <img style="width:70px;" src="assets/img/logo_black.png" alt="logo">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="main-navbar">
+          <div class="container">
+            <nav class="navbar navbar-expand-md navbar-light">
+              <a class="navbar-brand" href="home">
+                <img style="width:70px;" src="assets/img/logo_black.png" alt="logo">
+              </a>
+              <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a href="safari" class="nav-link">Safari <i class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a href="7day-perfect-combo-safari.php" class="nav-link">7 Days- Perfect Combo</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6Days-wildlife-safari.php" class="nav-link">6 Days-Wildlife DIscovery</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-restful-safari.php" class="nav-link">6 Days- Restfull</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="5days-lively-wildlife.php" class="nav-link">5 Days Lively Wildlife</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="5days-the-thrilling-predator.php"class="nav-link">5 Days- The thrilling</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a href="climbtrekk" class="nav-link">Climb & trekk<i class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a href="10days-lemosho-route.php" class="nav-link">10 Days Lemosho Route</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="9days-machame-route.php" class="nav-link">9 Days Machame Route</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="8days-rongai-route.php" class="nav-link">8 Days Rongai Route</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="7days-marangu-route.php" class="nav-link">7 Days Marangu Route</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link">6 Days mt Meru Route</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a href="daytrips.php" class="nav-link">Day Trips<i class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a href="gokarting.php" class="nav-link">Go Karting</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="chemka-hotsprings.php" class="nav-link">Chemka Hot Spring</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="Marangu-waterfalls.php" class="nav-link">Marangu Waterfalls</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="BBQ-street-food.php" class="nav-link">BBQ & Street Food</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link">Canoeing & Birdwatching</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link">City Tour</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="zipline1.php" class="nav-link">Zipline</a>
+                      </li>
+                      <!-- <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link">Arusha by Night</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link"> NATIVE TOUR/a>
+                      </li> -->
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a href="" class="nav-link">Special Tours<i class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a href="10days-lemosho-route.php" class="nav-link">Honeymoon</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="9days-machame-route.php" class="nav-link">Engagement</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="8days-rongai-route.php" class="nav-link">Customers Request</a>
+                      </li>
+                      <!-- <li class="nav-item">
+                        <a href="7days-marangu-route.php" class="nav-link">7 Days Marangu Route</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="6days-mt-meru-route.php" class="nav-link">6 Days mt Meru Route</a>
+                      </li> -->
+                    </ul>
+                  </li>
+                  <!-- <li class="nav-item">
+                    <a href="javascript:void(0);"
+                      class="nav-link">Trips <i
+                        class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">Day
+                          Trips <i
+                            class="fas fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">10 Days
+                              Lemosho Route</a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">9 Days
+                              Machame Route</a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">8 Days
+                              Rongai Route</a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">7 Days
+                              Marangu Route</a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">6 Days
+                              mt Meru Route</a>
+                          </li>
+                        </ul>
+                      </li>
+
+                      <li class="nav-item">
+                        <a href="#"
+                          class="nav-link">Special Trips <i
+                            class="fas fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                          <li class="nav-item">
+                            <a href="#"
+                              class="nav-link">Honeymoon</a>
+                          </li>
+                        </ul>
+                      </li> -->
+
+                    <!-- </ul>
+                  </li> -->
+                  <li class="nav-item">
+                    <a href="about"
+                      class="nav-link">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="contact"
+                      class="nav-link">Contact</a>
+                  </li>
+                </ul>
+                </li>
+                </ul>
+                <div class="others-options d-flex align-items-center">
+                  <div class="option-item">
+                    <a href="#" class="search-box">
+                      <i class="bi bi-search"></i>
+                    </a>
+                  </div>
+                  <div class="option-item">
+                    <a href="contact" class="btn  btn_navber">Book Now</a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div class="others-option-for-responsive">
+          <div class="container">
+            <div class="dot-menu">
+              <div class="inner">
+                <div class="circle circle-one"></div>
+                <div class="circle circle-two"></div>
+                <div class="circle circle-three"></div>
+              </div>
+            </div>
+            <div class="container">
+              <div class="option-inner">
+                <div class="others-options d-flex align-items-center">
+                  <div class="option-item">
+                    <a href="#" class="search-box"><i class="fas fa-search"></i></a>
+                  </div>
+                  <div class="option-item">
+                    <a href="contact" class="btn  btn_navber">Get free quote</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>

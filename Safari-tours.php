6843328286723a21de53e4c409ab5ad9146178e9
D94emil <dansonmwakireti@gmail.com>
2025-04-27 04:33:09 +0300

    Add file and make change at homepage

---
+++ Safari-tours.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Title -->
+    <title>Dreamy Exploration Kenya Safaris  - Explore wild with us.</title>
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <!-- animate css -->
+    <link rel="stylesheet" href="assets/css/animate.min.css" />
+    <!-- Fontawesome css -->
+    <link rel="stylesheet" href="assets/css/fontawesome.all.min.css" />
+    <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.8.2/font/bootstrap-icons.css"> <!-- owl.carousel css -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+    <!-- owl.theme.default css -->
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
+    <!-- navber css -->
+    <link rel="stylesheet" href="assets/css/navber.css" />
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="assets/css/meanmenu.css" />
+    <!-- Style css -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="assets/img/#">
+</head>
+
+<body>
+    <!-- preloader Area -->
+    <div class="preloader">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="lds-spinner">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Header Area -->
+     <?php
+      include "includes/navbar.php"
+     ?>
+    <!-- Header Area -->
+
+    <!-- search -->
+    <div class="search-overlay">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-close">
+                    <span class="search-overlay-close-line"></span>
+                    <span class="search-overlay-close-line"></span>
+                </div>
+                <div class="search-overlay-form">
+                    <form>
+                        <input type="text" class="input-search" placeholder="Search here...">
+                        <button type="button"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Common Banner Area -->
+    <section id="common_banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="common_bannner_text">
+                        <h2>Safari search result</h2>
+                        <ul>
+                            <li><a href="home">Home</a></li>
+                            <li><span><i class="fas fa-circle"></i></span> Tours</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Tour Search Areas -->
+    <section id="explore_area" class="section_padding">
+        <div class="container">
+            <!-- Section Heading -->
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="section_heading_center">
+                        <h2>Available Safari tours</h2>
+                    </div>
+                </div>
+            </div>
+
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img/im.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya</p>
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="7day-perfect-combo-safari.php">Maasai Mara National Park</a></h4>
+                                    <p><span class="review_rating">4.8/5 Excellent</span> <span
+                                            class="review_count">(1214
+                                            reviewes)</span></p>
+                                    <h3> <span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img//elephan.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya</p>
+                                    <div class="discount_tab">
+                                        <span></span>
+                                    </div>
+
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="6Days-wildlife-safari.php">Amboseli National Park</a></h4>
+                                    <p><span class="review_rating">4.8/5 Excellent</span> <span
+                                            class="review_count">(1214
+                                            reviewes)</span></p>
+                                    <h3><span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img//muz.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya</p>
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="tour-details">Tsavo East National Park</a></h4>
+                                    <p><span class="review_rating">4.8/5 Excellent</span> <span
+                                            class="review_count">(1214
+                                            reviewes)</span></p>
+                                    <h3><span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img//ostr.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya</p>
+                                    <div class="discount_tab">
+                                        <span></span>
+                                    </div>
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="tour-details">Samburu National Park</a></h4>
+                                    <p><span class="review_rating">4.8/5 Excellent</span> <span
+                                            class="review_count">(1214
+                                            reviewes)</span></p>
+                                    <h3><span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img//kade.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya</p>
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="tour-details">Meru National Park</a></h4>
+                                    <p><span class="review_rating">4.8/5 Excellent</span> <span
+                                            class="review_count">(1214
+                                            reviewes)</span></p>
+                                    <h3><span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="theme_common_box_two img_hover">
+                                <div class="theme_two_box_img">
+                                    <a href="tour-details"><img src="assets/img/holiday-img//zebra.png" alt="img"></a>
+                                    <p><i class="fas fa-map-marker-alt"></i>Kenya<p>
+                                </div>
+                                <div class="theme_two_box_content">
+                                    <h4><a href="tour-details">Design Your Safari trip</a></h4>
+                                    <p><span class="review_rating"></span> <span
+                                            class="review_count"></span></p>
+                                    <h3><span></span></h3>
+                                </div>
+                            </div>
+                        </div>
+    </section>
+
+
+    <!-- Cta Area -->
+    <section id="cta_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7">
+                    <div class="cta_left">
+                        <div class="cta_icon">
+                            <img src="assets/img/common/email.png" alt="icon">
+                        </div>
+                        <div class="cta_content">
+                            <h4>Get the latest news and offers</h4>
+                            <h2>Subscribe to our newsletter</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="cat_form">
+                        <form id="cta_form_wrappper">
+                            <div class="input-group"><input type="text" class="form-control"
+                                    placeholder="Enter your mail address"><button class="btn btn_theme btn_md"
+                                    type="button">Subscribe</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <!-- Footer  -->
+    <?php
+    include "includes/footer.php"
+    ?>
+    <!-- Footer  -->
+    
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <!-- Meanu js -->
+    <script src="assets/js/jquery.meanmenu.js"></script>
+    <!-- Range js -->
+    <script src="assets/js/nouislider.min.js"></script>
+    <script src="assets/js/wNumb.js"></script>
+    <!-- owl carousel js -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- wow.js -->
+    <script src="assets/js/wow.min.js"></script>
+    <!-- Custom js -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/add-form.js"></script>
+    <script src="assets/js/form-dropdown.js"></script>
+
+</body>
+
+</html>

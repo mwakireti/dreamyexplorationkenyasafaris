0f595b847c5429ba21f44e18b202cde1b5a0351f
D94emil <dansonmwakireti@gmail.com>
2025-04-27 04:40:53 +0300

    additional header

--- gokarting.php
+++ gokarting.php
@@ -4,22 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Title -->
-    <title>Nortaz Safari Tours </title>
+    <title>Dreamy Exploration Kenya Safaris  - Explore wild with us.</title>
     <!-- Bootstrap css -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <!-- animate css -->
     <link rel="stylesheet" href="assets/css/animate.min.css" />
     <!-- Fontawesome css -->
     <link rel="stylesheet" href="assets/css/fontawesome.all.min.css" />
-    <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.8.2/font/bootstrap-icons.css">
-    <!-- owl.carousel css -->
+    <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.8.2/font/bootstrap-icons.css"> <!-- owl.carousel css -->
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-    <!-- Slick css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/slick.min.css" />
-    <!--slick-theme.css-->
-    <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.min.css" />
-    <!-- Rangeslider css -->
-    <link rel="stylesheet" href="assets/css/nouislider.css" />
     <!-- owl.theme.default css -->
     <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
     <!-- navber css -->
@@ -31,7 +24,7 @@
     <!-- Responsive css -->
     <link rel="stylesheet" href="assets/css/responsive.css" />
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="assets/img/#">
 </head>
 
 <body>
